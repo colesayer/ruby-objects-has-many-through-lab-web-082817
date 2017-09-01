090233f84c2534fb949815de0b97b9cd319a55ee
colesayer <colesayershapiro@gmail.com>
2017-09-01 15:23:18 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
+
+  def initialize(date, doctor)
+    @date = date
+    doctor.add_appointment(self)
+  end
+
+  def doctor
+    @doctor
+  end
+
+end
